a41ea6f8d264fba322f143f2434b7a4196b2d50d
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2020-06-03 06:36:53 -0400

    Login pelo Contexto

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,24 +1,21 @@
-import React, { useRef, useCallback, useContext } from 'react';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
-import * as Yup from 'yup';
-
-import AuthContext from 'context/AuthContext';
-import getValidationErrors from 'utils/getValidationErrors';
-
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from 'assets/logo.svg';
-
 import Button from 'components/Button';
 import Input from 'components/Input';
-
-import { Container, Content, Background } from './styles';
+import { AuthContext } from 'context/AuthContext';
+import React, { useCallback, useContext, useRef } from 'react';
+import { FiLock, FiLogIn, FiMail } from 'react-icons/fi';
+import getValidationErrors from 'utils/getValidationErrors';
+import * as Yup from 'yup';
+import { ParamsFormData } from './ParamsFormData';
+import { Background, Container, Content } from './styles';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-  const { name } = useContext(AuthContext);
+  const { signIn } = useContext(AuthContext);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: ParamsFormData) => {
     try {
       formRef.current?.setErrors({});
       const schema = Yup.object().shape({
@@ -28,6 +25,7 @@ const SignIn: React.FC = () => {
         password: Yup.string().required('Senha obrigatória'),
       });
       await schema.validate(data, { abortEarly: false });
+      signIn({ email: data.email, password: data.password });
     } catch (error) {
       const errors = getValidationErrors(error);
       formRef.current?.setErrors(errors);
